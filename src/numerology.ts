9fb7ad5cba884076feb44fc50f02b225761859a8
Maria Soria <maria.it3@gmail.com>
2022-05-16 20:02:47 +0100

    :recycle: extract methods and variables

--- src/numerology.ts
+++ src/numerology.ts
@@ -1,20 +1,28 @@
+const parseToArray = (birthdate: Date): string[] => {
+  const [year, month, day] = [
+    birthdate.getFullYear().toString(),
+    (birthdate.getMonth() + 1).toString(),
+    birthdate.getDate().toString(),
+  ];
+  return (month + day + year).split('');
+};
+
+const hasMoreThanOneDigit = (yourNumber: number): boolean => yourNumber >= 10;
+
 export class Numerology {
   static calculateNumberFrom(birthdate: Date): number {
-    const [year, month, day] = [
-      birthdate.getFullYear().toString(),
-      (birthdate.getMonth() + 1).toString(),
-      birthdate.getDate().toString(),
-    ];
-    const date = month + day + year;
+    const dateInDigits = parseToArray(birthdate);
     let yourNumber = 0;
-    date.split('').forEach(elem => {
-      yourNumber += Number.parseInt(elem);
-      if (yourNumber >= 10) {
-        yourNumber =
-          Number.parseInt(yourNumber.toString()[0]) +
-          Number.parseInt(yourNumber.toString()[1]);
+    dateInDigits.forEach(digit => {
+      yourNumber += Number(digit);
+      if (hasMoreThanOneDigit(yourNumber)) {
+        yourNumber = this.addDigitsIn(yourNumber);
       }
     });
     return yourNumber;
+  }
+
+  private static addDigitsIn(yourNumber: number): number {
+    return Number(yourNumber.toString()[0]) + Number(yourNumber.toString()[1]);
   }
 }
